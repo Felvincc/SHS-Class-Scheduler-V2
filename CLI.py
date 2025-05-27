7679ef56bba30e2f1017709e1c236b3093748c4b
Felvincc <felvincedy1@gmail.com>
2025-05-28 05:54:07 +0800

    database rework
    chore (queries): changed the databases to standalone centralized no dynamic creating bs

--- CLI.py
+++ CLI.py
@@ -28,7 +28,7 @@ class MyCommand(cmd.Cmd):
 Usage: generate <save name>'''
         pass
 
-    def do_inputdata(self, arg):
+    def do_inputdataFAKE(self, arg):
         '''Starts the data gathering process
 Usage: inputdata'''
 
@@ -212,7 +212,194 @@ Usage: inputdata'''
         
         cursor.execute(f"INSERT INTO {save_name}_ampm_slots (am_slot, pm_slot) VALUES ('{amslots}', '{pmslots}')")
 
-     
+    def do_inputdata(self, arg):
+        '''Starts the data gathering process
+Usage: inputdata'''
+
+        condition = True
+        cls()
+
+        print("Please keep spelling the same and consistent.")
+        print("Write 'next', when you are finished entering the data for the current field.")
+
+        save_name = input("Enter save name> ")
+
+        cursor.execute(f"INSERT INTO saves (save_name) VALUES ('{save_name}')")
+        conn.commit()
+
+        instructors_data = [] # [[instructor, [subject, subject]],...]
+        while condition:
+            temp_instructor_subject = []
+            temp_instructor = input("Enter Instructor's name> ").lower()
+            
+            if temp_instructor == "next":
+                temp_instructor = []
+                break
+            else:
+                temp = []
+                temp_instructor_subject.append(temp_instructor)
+                while condition:
+                    subject = input("Enter the Subjects teach> ").lower()
+
+                    if subject == "next":
+                        subject = []
+                        temp_instructor_subject.append(temp)
+                        instructors_data.append(temp_instructor_subject)
+                        break
+                    else:
+                        temp.append(subject)
+
+        course_data = [] #[course, number of sections, [subject, subject, ...]]
+        while condition:
+            subjects = []
+            temp_course = input("Enter course name> ").lower()
+            if temp_course == "next":
+                temp_course = []
+                break
+            else:
+                temp_num_sections = int(input("Enter the number of sections in this course> "))
+
+                while condition:
+                    temp_subject = input("Enter the subjects in this course> ").lower()     
+
+                    if temp_subject == "next":
+                        temp_subject = []
+                        temp_course_data = [temp_course, temp_num_sections, subjects]
+                        course_data.append(temp_course_data)
+                        break
+                    else:
+                        subjects.append(temp_subject)
+
+        address_data = [] # [building, num_floors, [rooms], [...]]
+        while condition:
+            temp_building = input("Enter building name> ").lower()
+
+            if temp_building == "next":
+                break
+            else:
+                num_of_floors = int(input("Enter number of floors in the building> "))
+                rooms = []
+
+                for x in range(num_of_floors):
+                    while condition:
+                        temp_room = input(f"Enter the rooms in floor {x+1}> ").lower()
+                        if temp_room == "next":
+                            break
+
+                        else:
+                            rooms.append(temp_room)
+                
+                temp_address_data = [temp_building, num_of_floors, rooms]
+            
+            address_data.append(temp_address_data)
+
+        print(address_data)
+
+        amslots = int(input("Enter AM slots> "))
+        pmslots = int(input("Enter PM slots> "))
+
+        instructor_availability_condition = input("Do you want to add instructor availabilty? (Y/N)> ").lower()
+
+        if instructor_availability_condition == 'y':
+            pass
+        else:
+            pass
+
+        cursor.execute(f"SELECT * FROM saves WHERE save_name = '{save_name}'")
+        save_id = cursor.fetchall()[0][0]
+    
+
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS ampm(
+                ampm_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                save_id INTEGER NOT NULL,
+                FOREIGN KEY (save_id) REFERENCES saves(save_id) ON DELETE CASCADE
+                )
+            """)
+
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS courses(
+                course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                save_id INTEGER NOT NULL,
+                course_name TEXT NOT NULL,
+                num_sections INTEGER NOT NULL,
+                FOREIGN KEY (save_id) REFERENCES saves(save_id) ON DELETE CASCADE
+                )
+            """)
+        
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS subjects(
+                subject_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                save_id INTEGER NOT NULL,
+                subject_name TEXT NOT NULL UNIQUE,
+                FOREIGN KEY (save_id) REFERENCES saves(save_id) ON DELETE CASCADE) """)
+        
+        #junction table
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS course_subjects(
+                course_id INTEGER NOT NULL,
+                subject_id INTEGER NOT NULL,
+                PRIMARY KEY (course_id, subject_id),
+                FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
+                FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+                )
+            """)
+        
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS instructors(
+                instructor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                save_id INTEGER NOT NULL,
+                instructor_name TEXT NOT NULL,
+                FOREIGN KEY (save_id) REFERENCES saves(save_id) ON DELETE CASCADE
+                )
+            """)
+        
+        #junction table
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS instructor_subjects(
+                instructor_id INTEGER NOT NULL,
+                subject_id INTEGER NOT NULL,
+                PRIMARY KEY (instructor_id, subject_id),
+                FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id) ON DELETE CASCADE,
+                FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+                )
+            """)
+        
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS buildings(
+                building_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                save_id INTEGER NOT NULL,
+                building_name TEXT NOT NULL,
+                FOREIGN KEY (save_id) REFERENCES saves(save_id) ON DELETE CASCADE
+                ) 
+            """)
+        
+        cursor.execute(f""" 
+            CREATE TABLE IF NOT EXISTS rooms(
+                room_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                building_id INTEGER NOT NULL,
+                room_name TEXT NOT NULL,
+                FOREIGN KEY (building_id) REFERENCES buildings(building_id) ON DELETE CASCADE
+                )
+            """)
+    
+        conn.commit()
+        
+        for x in instructors_data:
+            instructor = x[0]
+            cursor.execute("INSERT INTO instructors (save_id, instructor_name) VALUES (?, ?)", (save_id, instructor))
+
+
+
+
+        
+
+
+
+
+
+        
+
     def do_seedata(self, arg):
         '''Shows data from a specific save\n
 Usage: 
